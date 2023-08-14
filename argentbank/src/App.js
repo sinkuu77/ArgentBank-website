68a658a2a939dca9ef121e2c7136a3fd47126745
Minyoung GWON <sinkuu@naver.com>
2023-08-14 15:29:55 +0200

    Ajouté le composant Footer

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -1,18 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
+import './App.scss'
 import  Home  from './pages/Home'
 import  LogIn  from './pages/LogIn'
 import  User  from './pages/User'
 import  Error  from './pages/Error'
+import Footer from './containers/Footer'
 
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/login' element={<LogIn />}/>
       <Route path='/user' element={<User />}/>
       <Route path='/404' element={<Error />}/>
     </Routes>
+    <Footer />
+    </>
   );
 }
 
---
+++ argentbank/src/containers/Footer/index.js
@@ -0,0 +1,9 @@
+import './style.scss'
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <p className='footer__text'>Copyright 2020 Argent Bank</p>
+        </footer>
+    )
+}
